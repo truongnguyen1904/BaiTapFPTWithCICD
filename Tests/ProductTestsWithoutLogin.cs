29b951d673ba4475e27456c8330e0fe7279a3ada
truongnguyen1904 <kietyeutu@gmail.com>
2025-07-23 13:11:29 +0700

    Update ProductTestsWithoutLogin.cs

--- Tests/ProductTestsWithoutLogin.cs
+++ Tests/ProductTestsWithoutLogin.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace BaiTapFPT.Tests
 {
-    [TestFixture]
+    [TestFixture, Category("ProductWithOutLogin")]
     public class ProductTestsWithoutLogin : BaseTest
     {
         private ProductsPage productsPage;
